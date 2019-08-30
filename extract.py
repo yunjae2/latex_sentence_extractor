73c92bb6a1c518da2c7f7b16c41134eb66616c26
Yunjae Lee <lyj7694@gmail.com>
2019-08-30 14:27:40 +0900

    Add script for merging multi-line sentences
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

---
+++ extract.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import sys
+import re
+
+def extract(strings):
+    sentence=""
+    for string in strings:
+        string = string.strip()
+        if not string:
+            continue
+
+        string = re.sub(r"~\\cite{[^{]*}", "", string)
+
+        if not sentence:
+            sentence = string
+        else:
+            sentence += " " + string
+
+        if sentence.endswith("."):
+            print(sentence)
+            sentence = ""
+
+
+def main():
+    if len(sys.argv) == 1:
+        print("usage: %s <text strings>" % sys.argv[0])
+        sys.exit(1)
+    extract(sys.stdin)
+
+
+if __name__ == "__main__":
+    main()
