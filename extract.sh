d23c84e9e771fecc1164af324dcef657a1c49695
Yunjae Lee <lyj7694@gmail.com>
2019-09-04 15:47:45 +0900

    extract: Save result in clipboard directly
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

--- extract.sh
+++ extract.sh
@@ -13,7 +13,6 @@ endline=$3
 cat $textfile | head -n $endline | tail -n +$startline > .temp.txt
 
 CALLEE_DIR=`dirname $0`
-$CALLEE_DIR/__extract.py < .temp.txt > test.txt
+$CALLEE_DIR/__extract.py < .temp.txt | xclip -selection clipboard
 
 rm .temp.txt
-gedit test.txt
